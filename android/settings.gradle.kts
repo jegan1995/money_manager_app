ec4b3250a96871251bca197345c59fbdc89e14f7
devops-jegan <devopsjegan@gmail.com>
2026-01-15 19:42:54 +0000

    Simplify settings.gradle.kts configuration

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,25 +1,8 @@
 pluginManagement {
-    val flutterSdkPath: String by settings
-
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
-    }
-
-    plugins {
-        id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
-        id("com.android.application") version "8.1.0" apply false
-        id("org.jetbrains.kotlin.android") version "1.9.10" apply false
-        id("com.google.gms.google-services") version "4.4.0" apply false
-    }
-
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.namespace == "dev.flutter") {
-                useModule("io.flutter:flutter-gradle-plugin:${requested.version}")
-            }
-        }
     }
 }
 
@@ -27,6 +10,7 @@ plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
 }
 
 include(":app")

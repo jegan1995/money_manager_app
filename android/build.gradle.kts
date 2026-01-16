dd8b679dad5bd8fecbcc019166259ffdc8519a7d
devops-jegan <devopsjegan@gmail.com>
2026-01-16 03:50:09 +0000

    Upgrade Kotlin to 2.0.21 for Firebase compatibility

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.5.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
         classpath("com.google.gms:google-services:4.4.0")
     }
 }

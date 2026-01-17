947ba687879945196c7d069b7b57959b74de4171
devops-jegan <devopsjegan@gmail.com>
2026-01-18 03:30:52 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,32 +7,19 @@ plugins {
 
 android {
     namespace = "com.jegan.money_manager_app"
-    compileSdk = 34
-    ndkVersion = "27.0.12077973"
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
-
+    compileSdk = 33
+    
     defaultConfig {
         applicationId = "com.jegan.money_manager_app"
         minSdk = 21
-        targetSdk = 34
-        versionCode = flutter.versionCode()
-        versionName = flutter.versionName()
-        multiDexEnabled = true
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
     }
 
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("debug")
-            isMinifyEnabled = false
-            isShrinkResources = false
         }
     }
 }
@@ -42,8 +29,6 @@ flutter {
 }
 
 dependencies {
-    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
-    implementation("com.google.firebase:firebase-analytics")
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     implementation("com.google.firebase:firebase-firestore")
-    implementation("androidx.multidex:multidex:2.0.1")
 }
